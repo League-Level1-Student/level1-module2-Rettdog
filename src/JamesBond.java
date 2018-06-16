108855e4b16f919774414524689d141e05abdee4
league <league@wts-imac-11.attlocal.net>
2018-06-16 10:39:13 -0700

    TeaParty, Vault, JamesBond

--- src/JamesBond.java
+++ src/JamesBond.java
@@ -1,4 +1,19 @@
 
 public class JamesBond {
-
+	public static void main(String[] args) {
+		JamesBond bond = new JamesBond();
+		Vault vault = new Vault(23452);
+		System.out.println(bond.findCode(vault));
+	}
+	int findCode(Vault vault){
+		int i;
+		int number=0;
+		for(i = 1000000;i>-1;i--) {
+		if(vault.tryCode(i)) {
+			number = i;
+		}
+		
+		}
+		return number;
+	}
 }
--- src/Vault.java
+++ src/Vault.java
@@ -1,4 +1,11 @@
 
 public class Vault {
-
+	
+	int unlock = 1000;
+	Vault(int code){
+		unlock=code;
+	}
+	boolean tryCode(int code) {
+		return code==unlock;
+	}
 }
